35749f8a5827bf8d341c95f01152ef1b87b0ff23
ohmoo <ohmoo@DESKTOP-89TLLNG>
2018-05-29 01:21:23 +0900

    20180528_home_1

--- src/javaStudy/javaTest.java
+++ src/javaStudy/javaTest.java
@@ -4,11 +4,18 @@ public class javaTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("111");
-		int a = 10;
 		
-		System.out.println(a);
+		int sum = 0;
+		int result = 0;
+		
+		for (int i = 0; i < 11; i++) {
+			sum = sum + i;
+		}
+		
+		System.out.println("1부터 10까지의 합은 "+sum+"입니다.");
 
 	}
+	
+	
 
 }
