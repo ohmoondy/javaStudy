8d978213fdbb3284deedda0639d402dbb1124865
ohmoo <ohmoo@DESKTOP-89TLLNG>
2018-05-29 01:23:13 +0900

    20180528_home_2

---
+++ src/javaStudy/javaTest02.java
@@ -0,0 +1,24 @@
+package javaStudy;
+
+import java.util.Scanner;
+
+public class javaTest02 {
+  /*
+	public static void main(String[] args) {
+		Scanner gugudan = new  Scanner(System.in);
+		int in_ = gugudan.nextInt();
+		
+		System.out.println("입력한값은 " +in_+"입니다.");
+
+	}
+  */
+	
+    public static void main(String[] args) {
+		Scanner sample = new Scanner(System.in);
+		
+		String char_ = sample.nextLine();
+		
+		System.out.println("입력한값은 "+char_);
+		
+	}
+}
