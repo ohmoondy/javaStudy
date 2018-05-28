d6e62d2df8a365d1e95c10b5bd5bc46bd0d0222d
ohjungim <ohmoondy@gmail.com>
2018-05-28 17:50:49 +0900

    20180526_1

--- src/javaStudy/javaTest.java
+++ src/javaStudy/javaTest.java
@@ -5,6 +5,9 @@ public class javaTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("111");
+		int a = 10;
+		
+		System.out.println(a);
 
 	}
 
